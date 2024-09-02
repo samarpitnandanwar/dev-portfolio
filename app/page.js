a8ebafb75a31d2e2faf133000b3e456448865b89
SAMARPIT NANDANWAR <161283408+samarpitnandanwar@users.noreply.github.com>
2024-09-02 19:54:36 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ async function getData() {
 
   while (true) {
     const res = await fetch(
-      `https://dev.to/api/articles?username=${personalData.devUsername}&page=${page}&per_page=300`
+      `https://dev.to/api/articles?username=${personalData.devUsername}&page=${page}&per_page=400`
     );
 
     if (!res.ok) {
