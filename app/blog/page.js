58f8e89f3bab5f7845b7d1196dfa57dae6507610
SAMARPIT NANDANWAR <161283408+samarpitnandanwar@users.noreply.github.com>
2024-09-05 21:05:11 +0530

    Update page.js

--- app/blog/page.js
+++ app/blog/page.js
@@ -9,7 +9,7 @@ async function getBlogs() {
 
   while (true) {
     const res = await fetch(
-      `https://dev.to/api/articles?username=${personalData.devUsername}&page=${page}&per_page=400`
+      `https://dev.to/api/articles?username=${personalData.devUsername}&page=${page}&per_page=500`
     );
 
     if (!res.ok) {
